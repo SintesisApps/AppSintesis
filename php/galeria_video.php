91c71680659aefc38061889695a5fb828a0edbf2
SinstesisApps <webdev_t6@sinstesisdigital.com.mx>
2013-04-30 13:38:02 -0500

    video

--- php/galeria_video.php
+++ php/galeria_video.php
@@ -10,8 +10,7 @@ $r_app=mysql_query($select_app,$conexion);
 $i=0;//cuenta el  video principal
 
 $html.='
-		<div id="videogaleria">
-      	<div class="VideoTituloPrincipal">
+		<div class="VideoTituloPrincipal">
         	<strong>Confía EPN en superar prospectivas de crecimiento</strong>
         </div>
             <object width="100%" height="49%">
@@ -21,7 +20,6 @@ $html.='
               <param value="transparent" name="wmode"/>
               <embed width="100%" height="49%" wmode="transparent"  allowfullscreen="true" allowscriptaccess="always" type="application/x-shockwave-flash" src="http://youtube.com/v/M885NmIBLlc?fs=1&amp;hl=es_ES&amp;rel=0&amp;autoplay=0&showinfo=0&modestbranding=1"/>
             </object>
-      </div>
 		';
 		
 
