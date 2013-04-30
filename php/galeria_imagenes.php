0e259d91f3200aa06eaafc67562ed6eefa6adcbe
SinstesisApps <webdev_t6@sinstesisdigital.com.mx>
2013-04-30 12:18:24 -0500

    galeria

--- php/galeria_imagenes.php
+++ php/galeria_imagenes.php
@@ -2,8 +2,8 @@
 /*Galeria de imagenes*/
 include("../../includes/conexion.php");
 include('../../funciones.php');
-
-$html='<ul id="Gallery" class="gallery">';
+/*
+$html='';
 
 $select_app="SELECT * FROM galeria_imagenes_contenido";
 $r_app=mysql_query($select_app,$conexion);
@@ -12,31 +12,17 @@ while($imagen=mysql_fetch_array($r_app))
 {
 	//verificamos si el archivo existe
 	$ruta="http://166.78.193.53/images/imagenes-galeria/";
-	if(file_exists("http://166.78.193.53/images/imagenes-galeria/globo2.jpg"))
+	if(file_exists("../../images/imagenes-galeria/".$imagen['imagen'].""))
 	{
 		$html.='<li><a href="http://166.78.193.53/images/imagenes-galeria/'.$imagen['imagen'].'"><img src="http://166.78.193.53/images/imagenes-galeria/'.$imagen['imagen'].'" alt="" width="150" height="150" /></a></li>';
-		}
-		else
-		{
-			$html.='<li><a href="http://166.78.193.53/images/imagenes-galeria/'.$imagen['imagen'].'"><img src="http://166.78.193.53/images/imagenes-galeria/'.$imagen['imagen'].'" alt="" width="150" height="150" /></a></li>';
-		}
+		
+	}
 		
 	
 	
 }
 
-$html.="</ul>";
-
-/*
-$html='<ul id="Gallery" class="gallery">
-    <li><a href="images/full/001.jpg"><img src="http://166.78.193.53/APPSintesis/imagenes/imagenes-articulos/1.jpg" alt="" /></a></li>
-    <li><a href="images/full/001.jpg"><img src="http://166.78.193.53/APPSintesis/imagenes/imagenes-articulos/1.jpg" alt="" /></a></li>
-	<li><a href="images/full/001.jpg"><img src="http://166.78.193.53/APPSintesis/imagenes/imagenes-articulos/1.jpg" alt="" /></a></li>
-	<li><a href="images/full/001.jpg"><img src="http://166.78.193.53/APPSintesis/imagenes/imagenes-articulos/1.jpg" alt="" /></a></li>
-	<li><a href="images/full/001.jpg"><img src="http://166.78.193.53/APPSintesis/imagenes/imagenes-articulos/1.jpg" alt="" /></a></li>
-  </ul>';
-  */
 echo $html;
-
+*/
 
 ?>
