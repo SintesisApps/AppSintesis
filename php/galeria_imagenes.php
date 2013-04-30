8da0d6371f09af309ead616a046c5a8b1853a348
SinstesisApps <webdev_t6@sinstesisdigital.com.mx>
2013-04-30 12:44:46 -0500

    videos

--- php/galeria_imagenes.php
+++ php/galeria_imagenes.php
@@ -2,7 +2,7 @@
 /*Galeria de imagenes*/
 include("../../includes/conexion.php");
 include('../../funciones.php');
-/*
+
 $html='';
 
 $select_app="SELECT * FROM galeria_imagenes_contenido";
@@ -14,7 +14,8 @@ while($imagen=mysql_fetch_array($r_app))
 	$ruta="http://166.78.193.53/images/imagenes-galeria/";
 	if(file_exists("../../images/imagenes-galeria/".$imagen['imagen'].""))
 	{
-		$html.='<li><a href="http://166.78.193.53/images/imagenes-galeria/'.$imagen['imagen'].'"><img src="http://166.78.193.53/images/imagenes-galeria/'.$imagen['imagen'].'" alt="" width="150" height="150" /></a></li>';
+		//$html.='<li><a href="http://166.78.193.53/images/imagenes-galeria/'.$imagen['imagen'].'"><img src="http://166.78.193.53/images/imagenes-galeria/'.$imagen['imagen'].'" alt="" width="150" height="150" /></a></li>';
+		$html.='<li><a href="http://166.78.193.53/images/imagenes-galeria/globo2.jpg"><img src="http://166.78.193.53/images/imagenes-galeria/globo2.jpg" width="150" height="150" alt="" /></a></li>';
 		
 	}
 		
@@ -23,6 +24,6 @@ while($imagen=mysql_fetch_array($r_app))
 }
 
 echo $html;
-*/
+
 
 ?>
