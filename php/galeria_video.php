b55db24827c3f48ab88bb8fcc286e4e511e4d4ef
SinstesisApps <webdev_t6@sinstesisdigital.com.mx>
2013-04-30 14:05:53 -0500

    video

--- php/galeria_video.php
+++ php/galeria_video.php
@@ -5,11 +5,14 @@ include('../../funciones.php');
 
 $html='';
 
-$select_app="SELECT * FROM galeria_videos  WHERE status='1' ORDER BY fecha DESC ";
+$select_app="SELECT * FROM galeria_videos  WHERE status='1' ORDER BY fecha DESC limit 1";
 $r_app=mysql_query($select_app,$conexion);
 $i=0;//cuenta el  video principal
 
-$html.='
+while($video=mysql_fetch_array($r_app))
+{
+	
+		$html.='<div id="videogaleria">
 		<div class="VideoTituloPrincipal">
         	<strong>Confía EPN en superar prospectivas de crecimiento</strong>
         </div>
@@ -19,9 +22,32 @@ $html.='
               <param value="always" name="allowscriptaccess"/>
               <param value="transparent" name="wmode"/>
               <embed width="100%" height="49%" wmode="transparent"  allowfullscreen="true" allowscriptaccess="always" type="application/x-shockwave-flash" src="http://youtube.com/v/M885NmIBLlc?fs=1&amp;hl=es_ES&amp;rel=0&amp;autoplay=0&showinfo=0&modestbranding=1"/>
-            </object>
+            </object></div>
 		';
+}
+
+$html.='<div class="contvideogaleriaimg"> 
+      	<div class="contvideogaleriaimg_secundario">';
+
+		$reg="SELECT * FROM galeria_videos WHERE id NOT IN (SELECT max(id) FROM galeria_videos) ORDER BY fecha DESC";
+		$registros=mysql_query($reg);
+		while($video2=mysql_fetch_array($registros))
+		{
+			$html.='
+			 <div class="VideoImagen">
+                <a href="http://youtube.com/v/IomwuczBqJY" onclick="return cambiarvideo(this.href,this.title)" title="Toma protesta de diputados"> <img src="http://img.youtube.com/vi/IomwuczBqJY/0.jpg" />
+                  <div class="titulo_video"> <strong>Toma protesta de diputados</strong> </div>
+                </a> 
+            </div> 
+			';
+		}
 		
+		
+		
+		
+$html.='</div></div>';
+
+
 
 
 echo $html;
