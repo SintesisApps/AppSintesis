ab1160cfb7844e419cf050bfc0b8e3cbfedaa224
SinstesisApps <webdev_t6@sinstesisdigital.com.mx>
2013-04-29 19:03:25 -0500

    galeria

--- php/galeria_imagenes.php
+++ php/galeria_imagenes.php
@@ -3,7 +3,25 @@
 include("../../includes/conexion.php");
 include('../../funciones.php');
 
+$html='<ul id="Gallery" class="gallery">';
 
+$select_app="SELECT * FROM galerias_imagenes_contenido";
+$r_app=mysql_query($select_app,$conexion);
+
+while($imagen=mysql_fetch_array($array_app))
+{
+	//verificamos si el archivo existe
+	$ruta=$url_dominio_.'/images/imagenes-galeria/'.$imagen['imagen'];
+	if (file_exists($ruta))
+	{
+		$html.='<li><a href="#"><img src="'.$ruta.'" alt="" /></a></li>';
+	}
+	
+}
+
+$html.="</ul>";
+
+echo $html;
 
 
 ?>
