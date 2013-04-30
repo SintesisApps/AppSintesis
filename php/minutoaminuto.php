328e3d8a15a30306e7df768a49e81a5604982cab
SinstesisApps <webdev_t6@sinstesisdigital.com.mx>
2013-04-30 18:14:24 -0500

    art

--- php/minutoaminuto.php
+++ php/minutoaminuto.php
@@ -16,25 +16,27 @@ while($f_app=mysql_fetch_assoc($r_app)):
 	
 	$r_ar=mysql_query($select_ar,$conexion);
 	while($f_ar=mysql_fetch_assoc($r_ar)):
-		$TituloSlideMiniaturas=$f_ar['titulo'];
-		$Id_SeccionSlideMiniaturas=$f_ar['id_seccion'];
-		$AutorSlideMiniaturas=$f_ar['autor'];
-		$NotaSlideMiniaturas=$f_ar['nota'];
-		$Fecha_CreacionSlideMiniaturas=$f_ar['fecha_creacion'];
+		$Titulo_nota=$f_ar['titulo'];
+		$Id_SeccionS=$f_ar['id_seccion'];
+		$Autor=$f_ar['autor'];
+		$Nota=$f_ar['nota'];
+		$Fecha_Creacion=$f_ar['fecha_creacion'];
 		
-		$TituloSlideMiniaturas=utf8_encode($TituloSlideMiniaturas);
-		$AutorSlideMiniaturas=utf8_encode($AutorSlideMiniaturas);
-		$NotaSlideMiniaturas=utf8_encode($NotaSlideMiniaturas);
+		$Titulo_nota=utf8_encode($Titulo_nota);
+		$Autor=utf8_encode($Autor);
+		$Nota=utf8_encode($Nota);
 		
-		$TituloSlideMiniaturas=substr($TituloSlideMiniaturas,0,100)."...";
-		$imagen=extraer_imagen($NotaSlideMiniaturas);
+		$Titulo_nota=substr($Titulo_nota,0,100)."...";
+		$imagen=extraer_imagen($Nota);
 		$imagen=utf8_decode($imagen);
 	endwhile;
-	
-	$html='
-		<div style="width:87%;margin-left:10px;">
+	/*
+	<a href="#nota" onclick="LeerNota('.$id_nota_app.')">
+	*/
+	$html.='
+		<div style="width:87%;margin-left:10px;"><a href="#nota" onclick="LeerNota('.$id_nota_app.')">
           <div style="display:inline-block; color: rgb(0,85,143);"> 10:49 PM </div>
-          <div style="display:inline-block; color: rgb(151,151,151);"> / Mantendrá Seproci vigilancia vigilancia... </div>
+          <div style="display:inline-block; color: rgb(151,151,151);"> / '.$Titulo_nota.'</div></a>
           <hr>
         </div>
 	';
@@ -42,6 +44,8 @@ while($f_app=mysql_fetch_assoc($r_app)):
 	
 
 endwhile;
+
+echo $html;
 
 
 ?>
