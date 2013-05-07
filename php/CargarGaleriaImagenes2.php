9272229b0158af37226f9b20e42362d9dbd3c60b
SinstesisApps <webdev_t6@sinstesisdigital.com.mx>
2013-05-06 20:49:37 -0500

    gallery

---
+++ php/CargarGaleriaImagenes2.php
@@ -0,0 +1,36 @@
+<?php
+echo '<script>
+
+		(function(window, $, PhotoSwipe){
+			
+			$(document).ready(function(){
+				
+				var options = {};
+				$("#Gallery_gal a").photoSwipe(options);
+			
+			});
+			
+			
+		}(window, window.jQuery, window.Code.PhotoSwipe));
+		
+</script>
+<li><a href="http://166.78.193.53/AppSintesis/images/full/001.jpg"><img src="http://166.78.193.53/AppSintesis/images/thumb/001.jpg" alt="Image 001" /></a></li>
+		<li><a href="http://166.78.193.53/AppSintesis/images/full/002.jpg"><img src="http://166.78.193.53/AppSintesis/images/thumb/002.jpg" alt="Image 002" /></a></li>
+		<li><a href="http://166.78.193.53/AppSintesis/images/full/003.jpg"><img src="http://166.78.193.53/AppSintesis/images/thumb/003.jpg" alt="Image 003" /></a></li>
+		<li><a href="http://166.78.193.53/AppSintesis/images/full/004.jpg"><img src="http://166.78.193.53/AppSintesis/images/thumb/004.jpg" alt="Image 004" /></a></li>
+		<li><a href="http://166.78.193.53/AppSintesis/images/full/005.jpg"><img src="http://166.78.193.53/AppSintesis/images/thumb/005.jpg" alt="Image 005" /></a></li>
+		<li><a href="http://166.78.193.53/AppSintesis/images/full/006.jpg"><img src="http://166.78.193.53/AppSintesis/images/thumb/006.jpg" alt="Image 006" /></a></li>
+		<li><a href="http://166.78.193.53/AppSintesis/images/full/007.jpg"><img src="http://166.78.193.53/AppSintesis/images/thumb/007.jpg" alt="Image 007" /></a></li>
+		<li><a href="http://166.78.193.53/AppSintesis/images/full/008.jpg"><img src="http://166.78.193.53/AppSintesis/images/thumb/008.jpg" alt="Image 008" /></a></li>
+		<li><a href="http://166.78.193.53/AppSintesis/images/full/009.jpg"><img src="http://166.78.193.53/AppSintesis/images/thumb/009.jpg" alt="Image 009" /></a></li>
+		<li><a href="http://166.78.193.53/AppSintesis/images/full/010.jpg"><img src="http://166.78.193.53/AppSintesis/images/thumb/010.jpg" alt="Image 010" /></a></li>
+		<li><a href="http://166.78.193.53/AppSintesis/images/full/011.jpg"><img src="http://166.78.193.53/AppSintesis/images/thumb/011.jpg" alt="Image 011" /></a></li>
+		<li><a href="http://166.78.193.53/AppSintesis/images/full/012.jpg"><img src="http://166.78.193.53/AppSintesis/images/thumb/012.jpg" alt="Image 012" /></a></li>
+		<li><a href="http://166.78.193.53/AppSintesis/images/full/013.jpg"><img src="http://166.78.193.53/AppSintesis/images/thumb/013.jpg" alt="Image 013" /></a></li>
+		<li><a href="http://166.78.193.53/AppSintesis/images/full/014.jpg"><img src="http://166.78.193.53/AppSintesis/images/thumb/014.jpg" alt="Image 014" /></a></li>
+		<li><a href="http://166.78.193.53/AppSintesis/images/full/015.jpg"><img src="http://166.78.193.53/AppSintesis/images/thumb/015.jpg" alt="Image 015" /></a></li>
+		<li><a href="http://166.78.193.53/AppSintesis/images/full/016.jpg"><img src="http://166.78.193.53/AppSintesis/images/thumb/016.jpg" alt="Image 016" /></a></li>
+		<li><a href="http://166.78.193.53/AppSintesis/images/full/017.jpg"><img src="http://166.78.193.53/AppSintesis/images/thumb/017.jpg" alt="Image 017" /></a></li>
+		<li><a href="http://166.78.193.53/AppSintesis/images/full/018.jpg"><img src="http://166.78.193.53/AppSintesis/images/thumb/018.jpg" alt="Image 018" /></a></li>'
+		
+?>
