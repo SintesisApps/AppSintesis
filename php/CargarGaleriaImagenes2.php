6be16a23ec365281b9ddacbbe932618c9b3014bf
SinstesisApps <webdev_t6@sinstesisdigital.com.mx>
2013-05-06 21:35:16 -0500

    dfgdfgd

--- php/CargarGaleriaImagenes2.php
+++ php/CargarGaleriaImagenes2.php
@@ -1,5 +1,5 @@
 <?php
-/*echo '<script>
+echo '<script>
 
 		(function(window, $, PhotoSwipe){
 			
@@ -14,7 +14,7 @@
 		}(window, window.jQuery, window.Code.PhotoSwipe));
 		
 </script>
-<li><a href="images/full/001.jpg"><img src="images/thumb/001.jpg" alt="Image 001" /></a></li>
-		<li><a href="images/full/002.jpg"><img src="images/thumb/002.jpg" alt="Image 002" /></a></li>';*/
-		echo'hola';
+<li>ertertertet</li>
+		<li>etertetet</li>';
+		
 ?>
