3a4be74cdc55f20629ddfad476d2cf94ab7e7f1d
SinstesisApps <webdev_t6@sinstesisdigital.com.mx>
2013-05-20 13:40:35 -0500

    dfsf

--- php/Nota_pr.php
+++ php/Nota_pr.php
@@ -227,7 +227,7 @@ $css='
 </script>
 ';
 
-$imagen_p="<div style=\'display:inline-block; overflow:hidden\'><div class=\'ImagenesNotaImagen\'><img src=\'http://166.78.193.53/APPSintesis/imagenes/imagenes-articulos/1.jpg\'><div class=\'ZoomImagenNota\'><a href=\'imagenes/imagenes-articulos/1.jpg\' ><img src=\'imagenes/iconos/azules/ampliar@2x.png\'></a></div><div class=\'MasImagenNota\'><img src=\'imagenes/iconos/azules/otras@2x.png\'></div></div></div><div style=\'display:inline-block; overflow:hidden\'><div class=\'ImagenesNotaImagen\'><img src=\'imagenes/imagenes-articulos/2.jpg\'><div class=\'ZoomImagenNota\'><a href=\'imagenes/imagenes-articulos/2.jpg\'><img src=\'imagenes/iconos/azules/ampliar@2x.png\'></a></div></div></div>";
+$imagen_p="<div style=\'display:inline-block; overflow:hidden\'><div class=\'ImagenesNotaImagen\'><img src=\'http://166.78.193.53/APPSintesis/imagenes/imagenes-articulos/1.jpg\'><div class=\'ZoomImagenNota\'><a href=\'http://166.78.193.53/APPSintesis/imagenes/imagenes-articulos/1.jpg\' ><img src=\'http://166.78.193.53/APPSintesis/imagenes/iconos/azules/ampliar@2x.png\'></a></div><div class=\'MasImagenNota\'><img src=\'http://166.78.193.53/APPSintesis/imagenes/iconos/azules/otras@2x.png\'></div></div></div><div style=\'display:inline-block; overflow:hidden\'><div class=\'ImagenesNotaImagen\'><img src=\'http://166.78.193.53/APPSintesis/imagenes/imagenes-articulos/2.jpg\'><div class=\'ZoomImagenNota\'><a href=\'http://166.78.193.53/APPSintesis/imagenes/imagenes-articulos/2.jpg\'><img src=\'http://166.78.193.53/APPSintesis/imagenes/iconos/azules/ampliar@2x.png\'></a></div></div></div>";
 $arr1[$i]=array(
 	'titulo' => $Titulo_p,
 					'sumario' => $css.$Sumario_p,
